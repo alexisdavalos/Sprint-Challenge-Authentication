37c1e01636074bb751967f101e705a6877018fd3
Alexis <alexisdavalos.tech@gmail.com>
2020-02-28 11:56:24 -0500

    MVP Reached: auth-router.js, authenticate-middleware.js and users-model.js finished

--- api/server.js
+++ api/server.js
@@ -2,17 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+//middleware
 const authenticate = require('../auth/authenticate-middleware.js');
+
+//routers
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
 
+//server
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use('/api/auth', logger, authRouter);
+server.use('/api/jokes', logger, authenticate, jokesRouter);
+
+//sanity check route
+server.get('/', logger,(req, res) => {
+    res.json({ api: "online" });
+})
 
 module.exports = server;
+
+
+function logger(req, res, next) {
+    console.log(
+        `[${new Date().toISOString()}] 
+            \n ${req.method} to ${req.url} from ${req.get('Origin')}`, {reqBody: req.body, reqHeaders: req.headers, resBody: res.body, resHeaders: res.headers} 
+            
+    );
+
+    next();
+}
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,20 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken'); //imports jsonwebtoken
+
+const { jwtSecret } = require('../config/secret.js'); //imports secret from config
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  console.log('checking token in middleware.js')
+  const {authorization} = req.headers;
+  (authorization) ? //if truthy
+
+    jwt.verify(authorization, jwtSecret, (err, decodedToken) =>{
+      (err) ? //if truthy
+      res.status(401).json({message: 'Invalid Credentails'}) : //else
+      (req.decodedToken = decodedToken, next())
+    }) : //else
+      
+      res.status(401).json({ you: 'shall not pass!' });
 };
